752448a3a0b2e15fae430d9f826625332585cebd
m <m@bitsnbites.eu>
2015-06-08 20:25:11 +0200

    Added command line options to chimg

--- src/chimg.cpp
+++ src/chimg.cpp
@@ -7,8 +7,11 @@
 //-----------------------------------------------------------------------------
 
 #include <cstdint>
+#include <cstring>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include <FreeImage.h>
 
@@ -17,32 +20,99 @@
 namespace {
 
 const int kDefaultQuality = 230;  // 0 = min quality, 255 = max quality.
-const bool kUseYCbCr = true;
 const int kNumChannels = 3;
+
+bool ArgToInt(const char *arg, int *result) {
+  std::string arg_str(arg);
+  try {
+    *result = std::stoi(arg_str);
+    return true;
+  } catch (...) {
+    std::cout << "Invalid integer expression: " << arg << "\n";
+    *result = 0;
+  }
+  return false;
+}
+
+struct Options {
+  Options() {
+    use_ycbcr = true;
+    quality = kDefaultQuality;
+    input_file = nullptr;
+    output_file = nullptr;
+  }
+
+  bool Parse(int argc, const char **argv) {
+    std::vector<const char *> file_names;
+
+    bool success = true;
+    for (int k = 1; k < argc && success; ++k) {
+      const char *arg = argv[k];
+
+      if (arg[0] == '-') {
+        // Parse options (starting with '-').
+        if (std::strcmp(arg, "-rgb") == 0) {
+          use_ycbcr = false;
+        } else if (std::strcmp(arg, "-q") == 0) {
+          if (k + 1 < argc && ArgToInt(argv[++k], &quality)) {
+            success = quality >= 0 && quality <= 255;
+            if (!success)
+              std::cout << "Invalid quality level: " << quality << "\n";
+          } else {
+            success = false;
+          }
+        } else {
+          std::cout << "Invalid option: " << arg << "\n";
+          success = false;
+        }
+      } else {
+        // Non-options are file names.
+        file_names.push_back(arg);
+      }
+    }
+
+    if (!success || file_names.size() != 2) {
+      std::cout << "Usage: " << argv[0] << " [options] image outfile\n";
+      std::cout << "Options:\n";
+      std::cout << " -q <quality> Set the quality (0-255)\n";
+      std::cout << " -rgb         Use RGB color space (instead of YCbCr)\n";
+      return false;
+    }
+
+    input_file = file_names[0];
+    output_file = file_names[1];
+
+    return true;
+  }
+
+  bool use_ycbcr;
+  int quality;
+  const char *input_file;
+  const char *output_file;
+};
 
 }  // namespace
 
 int main(int argc, const char **argv) {
-  if (argc < 3) {
-    std::cout << "Usage: " << argv[0] << " image outfile" << std::endl;
+  Options options;
+  if (!options.Parse(argc, argv)) {
     return 0;
   }
-  const char *input_file = argv[1];
-  const char *output_file = argv[2];
 
   FreeImage_Initialise();
 
   // Load the source image using FreeImage.
   FIBITMAP *bitmap;
   {
-    FREE_IMAGE_FORMAT format = FreeImage_GetFileType(input_file);
+    FREE_IMAGE_FORMAT format = FreeImage_GetFileType(options.input_file);
     if (format == FIF_UNKNOWN) {
-      std::cerr << "Unknown file format for " << input_file << std::endl;
+      std::cerr << "Unknown file format for " << options.input_file
+                << std::endl;
       return -1;
     }
-    FIBITMAP *bitmap_tmp = FreeImage_Load(format, input_file);
+    FIBITMAP *bitmap_tmp = FreeImage_Load(format, options.input_file);
     if (!bitmap_tmp) {
-      std::cerr << "Unable to load " << input_file << std::endl;
+      std::cerr << "Unable to load " << options.input_file << std::endl;
       return -1;
     }
 
@@ -63,8 +133,8 @@ int main(int argc, const char **argv) {
                    height,
                    pixel_stride,
                    kNumChannels,
-                   kDefaultQuality,
-                   kUseYCbCr);
+                   options.quality,
+                   options.use_ycbcr);
   }
   std::cout << "Compressed size: " << encoder.packed_size() << std::endl;
 
@@ -73,7 +143,8 @@ int main(int argc, const char **argv) {
 
   // Write packed data to a file.
   {
-    std::ofstream f(output_file, std::ofstream::out | std::ofstream::binary);
+    std::ofstream f(options.output_file,
+                    std::ofstream::out | std::ofstream::binary);
     f.write(reinterpret_cast<const char *>(encoder.packed_data()),
             encoder.packed_size());
   }
